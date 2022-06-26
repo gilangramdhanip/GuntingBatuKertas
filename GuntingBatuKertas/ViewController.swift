7308cb08791275d293fd97c855620013436ef59a
Gilang Ramdhani Putra <gramdhani66@gmail.com>
2022-06-26 21:52:51 +0700

    Initial Commit

---
+++ GuntingBatuKertas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuntingBatuKertas
+//
+//  Created by Gilang Ramdhani Putra on 26/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
